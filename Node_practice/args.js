7d5213067d4a8e01b23c4b730a0ce51e357e8eec
Kumar Tambe <kumartambe49@gmail.com>
2026-08-18 01:39:35 +0530

    added useful comments.

--- Node_practice/args.js
+++ Node_practice/args.js
@@ -7,3 +7,6 @@ console.log(args[3]) // second argument
 // args 1 is for path to script 
 // thats why we write node script.js 
 // our args start from agrs 2
+
+// event loop , plain console lgs execute before setTimeout even if pause is 0 ms. 
+// http module = modern day express , server , req,res etc
