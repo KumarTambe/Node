17b47a66e3e6dd0fd46429a4e3f8b3445afa2d46
Kumar Tambe <kumartambe49@gmail.com>
2026-08-18 01:38:40 +0530

    completed node revision

---
+++ Node_practice/args.js
@@ -0,0 +1,9 @@
+const args = process.argv
+
+console.log(args[2]) // first argument
+console.log(args[3]) // second argument
+
+// args 0 is for path to node 
+// args 1 is for path to script 
+// thats why we write node script.js 
+// our args start from agrs 2
